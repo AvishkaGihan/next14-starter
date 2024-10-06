3f55c04183c6babce982821fd8f62fe0cbcdb8f5
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-10-06 23:39:44 +0530

    Next.js Image Explained

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/26150745/pexels-photo-26150745/free-photo-of-green-grassland-with-trees-in-countryside.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
